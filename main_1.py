7404055e9a53955db94171f1afdf036a8ae83248
insightwalla <127964633+insightwalla@users.noreply.github.com>
2023-11-05 06:56:28 +0000

    taking off keys

--- main_1.py
+++ main_1.py
@@ -585,10 +585,10 @@ class FeedBackHelper:
                   doc[0].reference.delete()
                st.success('Review deleted successfully')
             
-            if c3_button.form_submit_button('Update', type='primary', use_container_width=True, key =f'form{from_real_to_fake[index]}{venue}'):
+            if c3_button.form_submit_button('Update', type='primary', use_container_width=True):
                   OnUpdateButton(review)
 
-            if c1_button.form_submit_button('Delete', type='secondary', use_container_width=True, key =f'del{from_real_to_fake[index]}{venue}'):
+            if c1_button.form_submit_button('Delete', type='secondary', use_container_width=True):
                OnDeleteSingleRev()   
       
       with venue_tab:
