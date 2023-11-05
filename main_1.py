a37451a7e63eb722525182b6c6c68117bced4123
insightwalla <127964633+insightwalla@users.noreply.github.com>
2023-11-05 09:21:07 +0000

    handle empty df

--- main_1.py
+++ main_1.py
@@ -700,6 +700,9 @@ class FeedBackHelper:
             data_list.append(review.to_dict())
 
       df = pd.DataFrame(data_list)
+      if len(data) == 0:
+         st.info('Need to Upload some Data First')
+         st.stop()
       df = df.sort_values(by=['idx'])
       #st.write(len(df))
 
